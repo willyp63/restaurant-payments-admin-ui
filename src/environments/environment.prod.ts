1e78f9adca80ec1fd3e23eaca09b62fa67e7dd13
Wil Pirino <wpirino1@gmail.com>
2019-05-04 15:30:35 -0600

    wss

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://restaurant-payments-api.herokuapp.com',
-  apiWsUrl: 'ws://restaurant-payments-api.herokuapp.com',
+  apiWsUrl: 'wss://restaurant-payments-api.herokuapp.com',
 };
