b03078fb244c0c7239b325ddd68cc3ae5770ae20
Wil Pirino <wpirino1@gmail.com>
2019-05-05 09:32:00 -0600

    dont let socket close

--- src/app/services/websocket/websocket.service.ts
+++ src/app/services/websocket/websocket.service.ts
@@ -5,13 +5,17 @@ import { environment } from 'src/environments/environment';
 @Injectable({ providedIn: 'root' })
 export class WebsocketService {
 
-  private socket: WebSocket = new WebSocket(environment.apiWsUrl);
+  private socket: WebSocket;
 
   emit(eventName: string, data: any) {
+    if (!this.socket) { this.initSocket(); }
+
     this.socket.send(JSON.stringify({ event: eventName, data }));
   }
 
   on(eventName: string, onEvent: Function) {
+    if (!this.socket) { this.initSocket(); }
+
     this.socket.addEventListener('message', (event: MessageEvent) => {
       const eventData = JSON.parse(event.data);
 
@@ -19,6 +23,15 @@ export class WebsocketService {
         onEvent(eventData['data']);
       }
     });
+  }
+
+  private initSocket() {
+    console.log('INIT SOCKET');
+    this.socket = new WebSocket(environment.apiWsUrl);
+    this.socket.onclose = () => {
+      console.log('SOCKET CLOSED ON ITS OWN');
+      setTimeout(this.initSocket, 500);
+    };
   }
   
 }
